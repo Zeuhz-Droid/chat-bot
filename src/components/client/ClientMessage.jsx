cb925f888811eeb5484b0d75c3468001a20d2ede
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-09 08:55:12 +0100

    the client message component

---
+++ src/components/client/ClientMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Comp.css";
+
+const ClientMessage = (props) => {
+  return (
+    <div className="ClientMessage">
+      <p className="client-text">55</p>
+    </div>
+  );
+};
+
+export default ClientMessage;
