f7c738e55d8ab578311ffc3b9ad08d3df99e4347
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-09 08:55:21 +0100

    the server message component

---
+++ src/components/server/ServerMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Comp.css";
+
+const ServerMessage = (props) => {
+  return (
+    <div className="ServerMessage">
+      <div className="server-text">56</div>
+    </div>
+  );
+};
+
+export default ServerMessage;
